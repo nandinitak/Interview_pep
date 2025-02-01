c415315f39cd9888e7c6ec0fa1559d240916f7e8
nandinitak <158013434+nandinitak@users.noreply.github.com>
2025-02-01 15:00:49 +0530

    Time: 7 ms (10.63%), Space: 113.7 MB (71.9%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -3,21 +3,21 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n = nums.size();
         int sum=0;
-        int l =0;
-        int e= k-1;
+        // int l =0;
+        // int e= k-1;
         double av;
         int maxi= INT_MIN;
         if(n==1 && k==1){
             return nums[0];
         }
-       for(int i=0; i<n;i++){
+       for(int i=0; i<k;i++){
         sum+=nums[i];
-        if(i==e){
-            maxi=max(maxi,sum);
-            sum-=nums[l];
-            l++;
-            e++;
-        }
+
+       }
+       maxi=sum;
+       for(int i=k;i<n; i++){
+        sum+=nums[i]-nums[i-k];
+        maxi=max(maxi,sum);
        }
        av=(double)maxi/k;
        return av;
