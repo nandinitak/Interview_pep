b9709d2bf8d5d1c4c6408da78eac303be100720d
nandinitak <158013434+nandinitak@users.noreply.github.com>
2025-02-21 00:31:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
-    void f(int n, string curr, vector<string> &ans) {
-        if (curr.size() > n) return;
-        if (curr.size() == n) {
-            ans.push_back(curr);
-            return;
-        }
-
-        f(n, curr + '0', ans);
-        f(n, curr + '1', ans);
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
         int n = nums.size();
-        int s = nums[0].length();
-        unordered_map<string, int> mpp;
-        
-        // Fix: Correct insertion into unordered_map
-        for (auto &str : nums) {
-            mpp[str]++;
-        }
-
-        vector<string> ans;
-        string curr;
-        f(s, curr, ans);  // Fix: Use 's' instead of 'n' to generate correct length strings
-
-        // Find a missing binary string
-        for (auto &str : ans) {
-            if (mpp.find(str) == mpp.end()) {
-                return str;  // Return the first unique string found
+        string s="";
+        for(int i=0;i<n;i++){
+            if(nums[i][i]=='1'){
+                s+='0';
+            }else{
+                s+='1';
             }
         }
-
-        return ""; // Just in case, though the function should always find a unique string
+        return s;
     }
 };
