b6738393224e011340e8c92ccf0e2c26c4826e8d
nandinitak <158013434+nandinitak@users.noreply.github.com>
2025-02-21 23:52:23 +0530

    Time: 7 ms (39.34%), Space: 27.7 MB (22.96%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -15,16 +15,16 @@ using namespace std;
 
 class Solution {
 public:
-    int findPosition(vector<int>& in, int element, int n) {
-        for (int i = 0; i < n; i++) {
-            if (in[i] == element) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    // int findPosition(vector<int>& in, int element, int n, unordered_map<int,int>mpp) {
+    //     for (int i = 0; i < n; i++) {
+    //         if (in[i] == element) {
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
 
-    TreeNode* solve(vector<int>& pre, vector<int>& in, int inorderS, int inorderE, int& index, int n) {
+    TreeNode* solve(vector<int>& pre, vector<int>& in, int inorderS, int inorderE, int& index, int n, unordered_map<int,int> &mpp) {
         if (index >= n || inorderS > inorderE) {
             return nullptr;
         }
@@ -33,10 +33,10 @@ public:
         index++;  // Move to the next preorder element
 
         TreeNode* root = new TreeNode(element);
-        int position = findPosition(in, element, n);
+        int position = mpp[element];
 
-        root->left = solve(pre, in, inorderS, position - 1, index, n);
-        root->right = solve(pre, in, position + 1, inorderE, index, n);
+        root->left = solve(pre, in, inorderS, position - 1, index, n,mpp);
+        root->right = solve(pre, in, position + 1, inorderE, index, n,mpp);
 
         return root;
     }
@@ -46,6 +46,10 @@ public:
         int inorderS = 0;
         int inorderE = n - 1;
         int index = 0;
-        return solve(pre, in, inorderS, inorderE, index, n);
+        unordered_map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[in[i]]=i;
+        }
+        return solve(pre, in, inorderS, inorderE, index, n,mpp);
     }
 };
