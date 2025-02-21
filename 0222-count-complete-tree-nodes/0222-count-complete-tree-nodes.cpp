ded4d2ccb10328e911dd790fab2a44c8260cafe9
nandinitak <158013434+nandinitak@users.noreply.github.com>
2025-02-21 21:36:19 +0530

    Time: 0 ms (100%), Space: 31.2 MB (74.33%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int getheightL(TreeNode* node) {
         if (node == NULL) return 0;
-        return 1 + getheightL(node->left); // Traverse left to get height
+        return 1 + getheightL(node->left); 
     }
     int getheightR(TreeNode* node) {
         if (node == NULL) return 0;
-        return 1 + getheightR(node->right); // Traverse left to get height
+        return 1 + getheightR(node->right);
     }
 
     int countNodes(TreeNode* root) {
         if (root == NULL) return 0;
 
-        int h = 1; // Start from 1 to count the current node
+        int h = 1; 
         int h1 = getheightL(root->left);
         int h2 = getheightR(root->right);
 
-        if (h1 == h2) return pow(2, h1 + 1) - 1; // Correct formula for full complete tree
+        if (h1 == h2) return (1<<(h1 + 1)) - 1; // Correct formula for full complete tree
 
-        h += countNodes(root->left);  // Accumulate left subtree count
-        h += countNodes(root->right); // Accumulate right subtree count
+        h += countNodes(root->left);  
+        h += countNodes(root->right);
 
         return h;
     }
