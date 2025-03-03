cabb6579fb7b367f93b12bc3485dba6ec4b103b1
Nandini Tak <158013434+nandinitak@users.noreply.github.com>
2025-03-03 16:33:49 +0530

    Time: 7 ms (59.22%), Space: 133.4 MB (51.98%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int> ans(n);
-        stack<int> s;
+        vector<int> ans;
+        // stack<int> s;
         for(int i=0;i<n;i++){
             if(nums[i]<pivot){
-                s.push(nums[i]);
+                ans.push_back(nums[i]);
             }
         }
          for(int i=0;i<n;i++){
             if(nums[i]==pivot){
-                s.push(nums[i]);
+                ans.push_back(nums[i]);
             }
          }
           for(int i=0;i<n;i++){
             if(nums[i]>pivot){
-                s.push(nums[i]);
+                ans.push_back(nums[i]);
             }
         }
-        for(int i=n-1;i>=0;i--){
-            int e= s.top();
-            s.pop();
-            ans[i]=e;
-        }
+        // for(int i=n-1;i>=0;i--){
+        //     int e= s.top();
+        //     s.pop();
+        //     ans[i]=e;
+        // }
         return ans;
     }
 };
