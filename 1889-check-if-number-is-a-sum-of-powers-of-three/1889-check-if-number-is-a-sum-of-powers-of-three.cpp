d83a3b5b12ff81d597161eba8face22173dbd2fe
Nandini Tak <158013434+nandinitak@users.noreply.github.com>
2025-03-05 01:57:54 +0530

    Time: 5 ms (12.5%), Space: 8.3 MB (9.2%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    bool checkPowersOfThree(int n) {
-        int sum=0;
-
-        while(n>0){
-            if(n%3==2) return false;
-            n/=3;
+    bool check(int i,vector<int>&arr,int n,int sum , int target){
+        if(i==n){
+            return target==sum;
         }
-        return true;
-        
+        sum+=arr[i];
+        if(check(i+1,arr,n,sum,target)==true){
+            return true;
+        }
+        sum-=arr[i];
+        if(check(i+1,arr,n,sum,target)==true){
+            return true;
+        }
+        return false;
+    }
+    bool checkPowersOfThree(int n) {
+        int k = 0;
+        int power = 1; 
+        while (power * 3 <= n) {
+            power *= 3;
+            k++;
+        }
+        vector<int>arr;
+        for(int i=0;i<=k;i++){
+            arr.push_back(pow(3,i));
+        }
+        return check(0,arr,arr.size(),0,n);
     }
 };
