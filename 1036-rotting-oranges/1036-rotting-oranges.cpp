2965a19b3bd1db105754f4c20358332d86cf2e4d
Nandini Tak <158013434+nandinitak@users.noreply.github.com>
2025-03-06 18:08:29 +0530

    Time: 0 ms (100%), Space: 17.2 MB (27.73%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,60 +1,52 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int n = grid.size(), m = grid[0].size();
-        int cntfresh = 0;
-        
-        queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0)); // Correct initialization
-        
-        // Push initial rotten oranges into queue
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 2;
-                } else {
-                    vis[i][j] = 0;
-                }
-                if(grid[i][j] == 1) cntfresh++;
+       if(grid.empty()) return 0;
+       int n=grid.size(), m = grid[0].size();
+       int cntfresh=0;
+     
+       queue<pair<pair<int,int>,int>> q;
+       vector<vector<int>> vis(n,vector<int>(m,0));
+    //    here we are pushing oth level rotten oranges
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+                 q.push({{i,j},0});
+                 vis[i][j]=2;
+
+            }else{
+                vis[i][j]=0;
             }
+            if(grid[i][j]==1) cntfresh++;
         }
+       }
+    //    checking neighbours for 1 if they then we are pushing them to queue and there vis will be 2; also increasing time;
+       int tm=0;
+       int drow[]={-1,0,+1,0};
+       int dcol[]={0,1,0,-1};
+         int cnt=0;
+       while(!q.empty()){
+        int r=q.front().first.first;
+        int c=q.front().first.second;
+        int t=q.front().second;
+        tm=max(tm,t);
+        q.pop();
+        for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                q.push({{nrow,ncol},t+1});
+                vis[nrow][ncol]=2;
+                  cnt++;
 
-        // Movement arrays
-        int drow[] = {-1, 0, +1, 0};
-        int dcol[] = {0, +1, 0, -1};
-        
-        int tm = 0;
-        int cnt = 0;
-
-        // BFS traversal
-        while(!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm, t);
-            q.pop();
-
-            for(int i = 0; i < 4; i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-                    q.push({{nrow, ncol}, t + 1});
-                    vis[nrow][ncol] = 2;
-                    cnt++;
-                }
             }
+             
         }
+       }
 
-        // Check if all fresh oranges have rotted
-        if(cnt != cntfresh) return -1;
-        return tm;
+    //    now we are checking if all the orranges are rotten or not after final time of (queue is empty).
+   if(cnt!=cntfresh) return -1;
+    return tm;
+
     }
 };
-
-
