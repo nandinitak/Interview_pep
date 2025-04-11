a66d7fb88d6e27966c6d3a2aee911647923981b6
Nandini Tak <158013434+nandinitak@users.noreply.github.com>
2025-04-11 15:16:11 +0530

    Time: 0 ms (100%), Space: 24.4 MB (67.58%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,31 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
-class Solution {
+
+    class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-    if (!root) return -1;
+    int count = 0;
 
-    queue<TreeNode*> q;
-    vector<int> values;
+    TreeNode* node(TreeNode* root, int k) {
+        if (!root) return NULL;
 
-    q.push(root);
+        TreeNode* left = node(root->left, k);
+        if (left) return left;
 
-    while (!q.empty()) {
-        TreeNode* node = q.front();
-        q.pop();
+        count++;
+        if (count == k) return root;
 
-        values.push_back(node->val);
-
-        if (node->left) q.push(node->left);
-        if (node->right) q.push(node->right);
+        TreeNode* right = node(root->right, k);
+        return right;
     }
 
-    sort(values.begin(), values.end()); 
-
-    return values[k - 1]; 
-}
+    int kthSmallest(TreeNode* root, int k) {
+        TreeNode* result = node(root, k);
+        return result ? result->val : -1;
+    }
 };
