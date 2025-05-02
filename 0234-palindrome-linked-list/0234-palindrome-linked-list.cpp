7482946ca25495bbedaab7428c5c6551da46c21a
Nandini Tak <158013434+nandinitak@users.noreply.github.com>
2025-05-02 15:46:28 +0530

    Time: 0 ms (100%), Space: 117.9 MB (83.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,4 +1,53 @@
-// 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* Middle(ListNode* head){
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast && fast->next){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         return slow;
+//     }
+//     ListNode* ReverseLinklist(ListNode* head){
+//         ListNode* prev= nullptr;
+//         while(head){
+//             ListNode* Next=head->next;
+//             head->next=prev;
+//              prev=head;
+//             head=Next;
+//         }
+//         return prev;
+//     }
+//     bool isPalindrome(ListNode* head) {
+//           if (!head || !head->next) return true;
+
+//         ListNode *middle=Middle(head);
+//         bool ispalin=false;
+//         ListNode* head2=ReverseLinklist(middle);
+//         ListNode* temp=head;
+//         while(head2){
+//             if(temp->val!=head2->val)
+//             ispalin=false;
+//             else{
+//                 ispalin=true;
+//             }
+//             temp=temp->next;
+//             head2=head2->next;
+//         }
+//         return ispalin;
+//     }
+// };
 class Solution {
 public:
     ListNode* Middle(ListNode* head) {
